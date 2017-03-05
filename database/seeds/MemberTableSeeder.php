44d65b721922d7e513ddad1ff48c9d7953b86315
Omar Abdel-Wahab <owahab@gmail.com>
2017-03-05 13:17:03 +0100

    Fixes seed error: Field 'password' doesn't have a default value

--- database/seeds/MemberTableSeeder.php
+++ database/seeds/MemberTableSeeder.php
@@ -11,12 +11,13 @@ class MemberTableSeeder extends Seeder
      */
     public function run()
     {
-           
+
           DB::table('t_members')->insert([
             'member_name' =>'Hisham Kharma',
             'member_image' =>'Hisham.jpg',
             'member_email' =>'HishamKharma@gmail.com',
             'member_mobile' =>'01001440701',
+            'password' => '12345678',
             'role_id'=>1,
             'created_at' =>  date("Y-m-d H:i:s"),
             'updated_at' =>  date("Y-m-d H:i:s"),
