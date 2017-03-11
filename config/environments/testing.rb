89a60449c771ca963b491d1afcaa0b77a026a306
Mohamed Elfiky <mohamed.elfikyy@yahoo.com>
2017-03-11 23:52:22 +0200

    add mail config to environments + create testing env for heroku

--- config/environments/testing.rb
+++ config/environments/testing.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'law3andakdam.com' }
+  config.action_mailer.default_url_options = { host: 'lawandakdam.herokuapp.com' }
 end
