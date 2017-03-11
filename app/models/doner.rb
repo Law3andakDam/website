e1348bcae477e9ca400fba437b72938733b89a20
Mohamed Elfiky <mohamed.elfikyy@yahoo.com>
2017-03-11 14:52:14 +0200

    doner valiations

--- app/models/doner.rb
+++ app/models/doner.rb
@@ -1,5 +1,9 @@
 class Doner < ApplicationRecord
   belongs_to :blood_type
+  
+  validates :mobile, :email, :blood_type_id, presence: true
+  validates :email, format: { with: Devise.email_regexp }
+
   after_create :send_confirmation_mail
 
   def send_confirmation_mail
--- spec/models/doner_spec.rb
+++ spec/models/doner_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Doner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:mobile) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:blood_type_id) }
+  it { should allow_value('elfiky@gmail.com').for(:email) }
+  it { should_not allow_value('elfiky.gmail').for(:email) }
+  it { should belong_to(:blood_type) }
 end
