3ec6f747bfe5032f436e7f303f64dc4922dc6a42
Mohamed Elfiky <mohamed.elfikyy@yahoo.com>
2017-03-11 14:46:10 +0200

    case model validations

--- spec/models/case_spec.rb
+++ spec/models/case_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Case, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:telephone) }
+  it { should validate_presence_of(:hospital) }
+  it { should validate_presence_of(:blood_type_id) }
+  it { should allow_value('elfiky@gmail.com').for(:email) }
+  it { should_not allow_value('elfiky.gmail').for(:email) }
+  it { should belong_to(:blood_type) }
 end
