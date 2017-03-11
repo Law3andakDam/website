5421cb517060d52880341be14285029780e9e9ee
Mohamed Elfiky <mohamed.elfikyy@yahoo.com>
2017-03-11 14:15:44 +0200

    add blood types relations and validation

--- spec/models/blood_type_spec.rb
+++ spec/models/blood_type_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BloodType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should have_many(:doners) }
+  it { should have_many(:cases) }
 end
