5825d7b0490952ac5b5c179521a58650f962f842
Mohamed Elfiky <mohamed.elfikyy@yahoo.com>
2017-03-12 02:33:17 +0200

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+BloodType.where(name: 'A+').first_or_create
+BloodType.where(name: 'A-').first_or_create
+BloodType.where(name: 'B+').first_or_create
+BloodType.where(name: 'B-').first_or_create
+BloodType.where(name: 'O+').first_or_create
+BloodType.where(name: 'O-').first_or_create
+BloodType.where(name: 'AB+').first_or_create
+BloodType.where(name: 'AB-').first_or_create
+
+User.where(email: 'admin@law3andakdam.com').first_or_create({
+  name: 'admin',
+  role: User::roles[:admin],
+  password: 'ChangeMe!',
+  confirmed_at: Time.now
+  })
